9fb536e1f1e8fceac85e2ffb4c67bd01d23a8f4e
sesamestrong <doolie.healey@gmail.com>
2020-03-19 01:51:21 -0400

    Add fix for long sessions

--- background.js
+++ background.js
@@ -2,13 +2,12 @@ let communicatingFrames = [];
 
 chrome.pageAction.onClicked.addListener(function(tab) {
     //TODO Tell iframe in current tab to toggle PiP
-    const frameToDo=communicatingFrames.find(frameInfo=>frameInfo.tabId==tab.id);
-    if(frameToDo){
-        chrome.tabs.sendMessage(tab.id,{type:'toggle'},{frameId:frameToDo.frameId});
-    }
+    const framesToDo=communicatingFrames.filter(frameInfo=>frameInfo.tabId==tab.id);
+    framesToDo.forEach(frameToDo=>chrome.tabs.sendMessage(tab.id,{type:'toggle'},{frameId:frameToDo.frameId}));
 });
 
 chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
+    console.log("heard");
     if(message.type=="loaded"){ //TODO make this refer to the video loading
         communicatingFrames.push({tabId:sender.tab.id,frameId:sender.frameId});
         chrome.pageAction.show(sender.tab.id);
