5d707f145650df6ec0e6335ac3f95bc0dd3a8179
sesamestrong <doolie.healey@gmail.com>
2020-05-09 18:41:24 -0400

    Add support for non-unique selectors, and choose between videos by playing and muted status

--- content.js
+++ content.js
@@ -1,19 +1,24 @@
-chrome.runtime.sendMessage({type:"loaded"});
+chrome.runtime.sendMessage({
+    type: "loaded"
+});
 let videoEl;
 
-chrome.runtime.onMessage.addListener(function({type,selector}){
-    console.log(selector);
-    const el=document.querySelector(selector);
-    if(el){
-        if(el.requestPictureInPicture)
+chrome.runtime.onMessage.addListener(function({
+    type,
+    selector
+}) {
+    const els = [...document.querySelectorAll(selector)];
+    const el = (els.length? (els.length == 1 ? els[0] : ((els=>els.length==1?els:els.filter(el => !el.muted))(els.filter(el => !el.paused))?.[0] || els[0])):null);
+    if (el) {
+        if (el.requestPictureInPicture)
             el.requestPictureInPicture();
-        else if(el.captureStream){
-            videoEl=videoEl||document.createElement("video");
-            const srcObject=el.captureStream(30);
-            videoEl.srcObject=srcObject;
+        else if (el.captureStream) {
+            videoEl = videoEl || document.createElement("video");
+            const srcObject = el.captureStream(30);
+            videoEl.srcObject = srcObject;
             videoEl.play();
             videoEl.requestPictureInPicture();
         }
-        
+
     }
 });
